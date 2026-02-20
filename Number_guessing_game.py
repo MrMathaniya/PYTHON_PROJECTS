d8a396064296b7c3bf0d73995d5798a85f157188
MrMathaniya <amssre.14202@gmail.com>
2026-02-20 20:24:06 +0530

    initial commmit

--- Number_guessing_game.py
+++ Number_guessing_game.py
@@ -1,7 +1,18 @@
 from random import randint
 no=randint(1,10)
-guess=int(input("Guess a number between 1 and 10: "))
-if guess==no:
-    print("You guessed right!, The number was",no)
-else:
-    print("Opps! You guessed wrong, The number was",no)
+guesses=0
+
+while True:
+    guess=int(input("Guess a number between 1 and 10: "))
+    guesses+=1
+    if guess==no:
+        print("You guessed right!, The number was",no)
+        break
+    else:
+        # print("Opps! You guessed wrong, try again")
+        if guess>no:
+            print(f"The number is small than {guess}")
+        else:
+            print(f"THe number is greater than {guess}")
+
+print(f"you've guessed the number in {guesses} guesses!")
